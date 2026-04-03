3b3fdcea0aee708131192a25b3b919faa1706c54
Dheeraj Kumar <ui.dheerajkumar@gmail.com>
2026-04-03 13:58:15 +0530

    initial push

---
+++ database.js
@@ -0,0 +1,10 @@
+/**
+ * Go to mongodb website
+ * create a free M0 Cluster and deploy to AWS server
+ * Create a user
+ * get the connection string
+ * */ 
+
+require('mongodb')
+
+const URI = 'mongodb+srv://adminmongo:admin123@todo-application.uzlprgx.mongodb.net/'
