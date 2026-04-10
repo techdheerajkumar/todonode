2d983aac536f7834a00bbbc165d4379e76ad002c
Dheeraj Kumar <ui.dheerajkumar@gmail.com>
2026-04-10 12:31:07 +0530

    added put and delete APIs

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const { connectDB } = require('./config/database')
 const { User } = require('./models/user')
 const express = require('express');
 const app = express();
-
 const PORT = 3000;
 app.use(express.json());
 
@@ -27,8 +26,35 @@ app.post('/signup', async (req, res) => {
 // Created a Feed API that gets all the users
 app.get('/feed', async (req, res)=>{
     const getUsers = await User.find({})
-    console.log(getUsers)
     res.send(getUsers)
+})
+
+
+// Created a delete api that deletes it from the database based on the filter (name, id, email)
+app.delete('/signup', async (req, res)=>{
+    const getUserById = req.body.userId;
+    console.log(getUserById)
+    try{
+        await User.findByIdAndDelete(getUserById)
+        res.send(await User.find({}))
+    }
+    catch (err) {
+        res.status(400).send(`found an error = ${err}`)
+    }
+})
+
+// Update API
+
+app.put('/signup', async (req, res) =>{
+    const id = req.body.id;
+    const data = req.body;
+    try{
+        await User.findByIdAndUpdate(id, data)
+        res.send('User info updated successfully!')
+    }
+    catch(err){
+        res.send(`found an error => ${err}`)
+    }
 })
 
 
